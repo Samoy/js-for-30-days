685c211bcf54d24eb6350ade9a76eca5380bfda2
Samoy <samoy_young@163.com>
2024-06-05 09:10:22 +0800

    执行可取消的延迟函数: https://leetcode.cn/problems/timeout-cancellation/?envType=study-plan-v2&envId=30-days-of-javascript

--- test/promiseandtime/timeout-cancellation.test.ts
+++ test/promiseandtime/timeout-cancellation.test.ts
@@ -1,29 +1,37 @@
-import {test, describe, expect} from "vitest";
+import {test, describe, expect, vi, beforeAll, afterAll} from "vitest";
 import {cancellable} from "@/promiseandtime/timeout-cancellation";
 
 describe('Test timeout cancellation', () => {
+    beforeAll(() => {
+        vi.useFakeTimers(); // 在所有测试开始前启用假的定时器
+    });
+
+    afterAll(() => {
+        vi.clearAllTimers(); // 测试结束后清理
+    });
+
     test('Test case 1', () => {
         const cancelTimeMs = 50;
-        const cancelFn = cancellable((x: number) => x * 5, [2], 20);
+        const mock = vi.fn((x: number) => x * 5);
+        const cancelFn = cancellable(mock, [2], 20)
         setTimeout(cancelFn, cancelTimeMs);
-        setTimeout(() => {
-            expect(cancelFn()).toBe(10);
-        }, 20);
-    })
+        vi.runAllTimers();
+        expect(mock).toBeCalled();
+    });
     test('Test case 2', () => {
         const cancelTimeMs = 50;
-        const cancelFn = cancellable((x: number) => x ** 2, [2], 100);
+        const mock = vi.fn((x: number) => x ** 2);
+        const cancelFn = cancellable(mock, [2], 100)
         setTimeout(cancelFn, cancelTimeMs);
-        setTimeout(() => {
-            expect(cancelFn()).toBeUndefined();
-        }, 100);
-    })
+        vi.runAllTimers();
+        expect(mock).not.toHaveBeenCalled();
+    });
     test('Test case 3', () => {
         const cancelTimeMs = 100;
-        const cancelFn = cancellable((x1: number, x2: number) => x1 * x2, [2, 4], 30);
+        const mock = vi.fn((x1: number, x2: number) => x1 * x2);
+        const cancelFn = cancellable(mock, [2, 4], 30)
         setTimeout(cancelFn, cancelTimeMs);
-        setTimeout(() => {
-            expect(cancelFn()).toBe(8);
-        }, 30);
-    })
+        vi.runAllTimers();
+        expect(mock).toBeCalled();
+    });
 })
